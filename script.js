29c66dee7448c513a0580427e74e02a32a594db6
holinanov <lotjenaber@gmail.com>
2026-09-20 23:09:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const shareButton = document.getElementById("shareTwitter");
 
 // Days per month
 const daysInMonth = {
-  1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
+  1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30,
   7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31
 };
 
